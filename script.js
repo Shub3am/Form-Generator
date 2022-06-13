8e46b4575471b3d5cc10f4dd7d5c9cb346f64185
Shubham <shubhamvishwakarma0604@gmail.com>
2022-06-13 20:08:11 +0530

    Added Legacy Files

---
+++ script.js
@@ -0,0 +1,21 @@
+const TypeSelector = document.querySelector("#TypSelector")
+const LabelGenerator = document.querySelector("#label")
+const Submit = document.querySelector("#submit")
+const Adder = document.querySelector("#Data")
+
+Submit.addEventListener("click", (e) => {
+	if (e) {
+	const Type = TypeSelector.value
+	const Label = LabelGenerator.value
+	const div = document.createElement("div")
+	const dlabel = document.createElement("label")
+	dlabel.innerHTML = `<label>${label.value}</label>`
+	const dtype = document.createElement("input")
+	dtype.type = Type
+	div.appendChild(dlabel)
+	div.appendChild(dtype)
+	div.
+	Adder.appendChild(div)
+	}
+
+})
